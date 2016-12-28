030296ec60747b9823d550ec5e8adf79f784bf3d
ice <meltedise@gmail.com>
2016-12-28 17:57:03 +0900

    add dummy apis dummy/a, dummy/b and dummy/c
    rails s -b 0.0.0.0 -p 3010
    curl -i http://0.0.0.0:3010/dummy/a
    curl -i http://0.0.0.0:3010/dummy/b
    curl -i http://0.0.0.0:3010/dummy/c

--- app/controllers/dummy_controller.rb
+++ app/controllers/dummy_controller.rb
@@ -1,10 +1,18 @@
+# coding: utf-8
+
 class DummyController < ApplicationController
   def a
+    json = { comment: 'あああ' }
+    render json: json, status: :ok
   end
 
   def b
+    json = {}
+    render json: json, status: :ok
   end
 
   def c
+    json = { timestamp: Time.now.strftime('%Y-%m-%d %H:%M:%S') }
+    render json: json, status: :ok
   end
 end
